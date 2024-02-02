e269a59e9a52b79b0b39bbce587cbe5a1150e6b0
João <vitorjao56@gmail.com>
2024-02-02 17:04:41 -0300

    initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+
+
+import * as S from './styles'
+
+export function Home(){
+    return (
+        <S.StyledView>
+            <S.StyledText>Homefsss</S.StyledText>
+        </S.StyledView>
+    );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native'
+
+export const StyledView = styled.SafeAreaView`
+    flex: 1;
+    background-color: '#fff';
+    align-items: 'center';
+    justify-content: 'center';
+
+`;
+
+export const StyledText = styled.Text``;
